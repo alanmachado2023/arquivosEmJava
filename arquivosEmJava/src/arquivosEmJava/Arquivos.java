9495717f9fd83ad17fac943838a7c5d15e8b244d
alanm <alanm@PC-Gamer>
2023-03-13 21:35:25 -0300

    Manipulação de arquivos em java

---
+++ arquivosEmJava/src/arquivosEmJava/Arquivos.java
@@ -0,0 +1,56 @@
+package arquivosEmJava;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arquivos {
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setEmail("pessoa1@gmail.com");
+		pessoa1.setIdade("50");
+		pessoa1.setNome("Alan Machado");
+		
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setEmail("pessoa2@gmail.com");
+		pessoa2.setIdade("25");
+		pessoa2.setNome("Marcos Tadeu");
+		
+		
+		Pessoa pessoa3 = new Pessoa();
+		pessoa3.setEmail("pessoa3@gmail.com");
+		pessoa3.setIdade("40");
+		pessoa3.setNome("Maria Jesus");
+		
+		List<Pessoa> pessoas = new ArrayList<>();
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+		
+				
+		
+		File arquivo = new File("P:\\Workspace\\Eclipese_Java\\arquivosEmJava\\src\\Arquivo.csv");  //atribui um local para o arquivo ser gerado | basta mudar o formato do arquivo
+		
+		if (!arquivo.exists()) {
+			arquivo.createNewFile();
+		}
+		
+		FileWriter escrever_no_arquivo = new FileWriter(arquivo);    //objeto de escrever no arquivo
+		
+		for (Pessoa p : pessoas) {
+			escrever_no_arquivo.write(p.getNome()+ ";" + p.getEmail() + ";" + p.getIdade() + "\n");
+		}
+		
+			
+		escrever_no_arquivo.flush();   //força os dados serem escritos e disponibilizados
+		escrever_no_arquivo.close();   //fecha o arquivo
+		
+		
+	}
+
+}
